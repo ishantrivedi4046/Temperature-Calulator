0fb6d1825c4396cac1b803400fe0fa553f17ef45
Ishan Trivedi <sheshtrivedi8046@gmail.com>
2021-01-18 22:38:14 +0530

    commit 1

--- src/Components/Temprature.tsx
+++ src/Components/Temprature.tsx
@@ -1,33 +1,31 @@
-import React from 'react';
+import React from "react";
 
-interface Props{
-    scale :string;
-    temp : string;
-    changedTemp : (a:string,b:string)=> void
+interface Props {
+  scale: string;
+  temp: string;
+  changedTemp: (a: string, b: string) => void;
 }
-const temperature:React.FunctionComponent<Props> = (props)=>{
+const temperature: React.FunctionComponent<Props> = (props) => {
+  let heading: string = "";
 
-    let heading:string ='';
+  if (props.scale === "c") heading = "Celsius";
+  else heading = "Farenheit";
 
-     if(props.scale ==='c')
-        heading='Celsius';
-     else
-        heading = 'Farenheit';
-    
-     return (
-         <div className='ml-4'>
-             <p className='inline-block font-medium'>{heading} :</p>
-             <input 
-             type='text' 
-             className='ml-2 bg-red-200' 
-             value={props.temp} 
-             onChange={(e)=>{
-                 const newVal=e.target.value;
-                 const curScale=(heading==='Celsius')?'c':'f';
-                 return props.changedTemp(newVal,curScale);
-                }
-             }/>
-         </div>
-     );
-}
+  return (
+    <div className="ml-4">
+      <p className="inline-block font-medium">{heading} :</p>
+      <p>Something new added!</p>
+      <input
+        type="text"
+        className="ml-2 bg-red-200"
+        value={props.temp}
+        onChange={(e) => {
+          const newVal = e.target.value;
+          const curScale = heading === "Celsius" ? "c" : "f";
+          return props.changedTemp(newVal, curScale);
+        }}
+      />
+    </div>
+  );
+};
 export default React.memo(temperature);
